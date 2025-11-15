70d27856ced721b0f9747bb1be384dc63ab9012a
wamimi <nellycyberpro@gmail.com>
2025-11-15 14:29:12 +0300

    feat: integrate ThirdwebProvider into app layout
    - Wrap SolanaWalletProvider with ThirdwebProvider
    - Enable multi-chain wallet support across the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { SolanaWalletProvider } from './providers/SolanaWalletProvider'
+import { ThirdwebProvider } from './providers/ThirdwebProvider'
 import { AuthProvider } from './providers'
 
 const geistSans = Geist({
@@ -28,7 +29,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AuthProvider>
-          <SolanaWalletProvider>{children}</SolanaWalletProvider>
+          <ThirdwebProvider>
+            <SolanaWalletProvider>{children}</SolanaWalletProvider>
+          </ThirdwebProvider>
         </AuthProvider>
       </body>
     </html>
